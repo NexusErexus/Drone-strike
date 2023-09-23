d314bb54c03d6eff36091067d964142d0b60cd21
NexusErexus <uksus924@gmail.com>
2023-09-23 16:22:15 +0300

    Added rotation and improved the movement of the ship

--- Drone Strike/Assets/Scripts/PlayerController.cs
+++ Drone Strike/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speedFactor = 2.0f;
+    [SerializeField] float speedFactor = 18.0f;
     float xRange = 10f;
     float yRange = 8f;
+    [SerializeField] float positionPitchFactor = -2.0f;
+    [SerializeField] float controlPitchFactor = -15.0f;
+    [SerializeField] float positionYawFactor = -.3f;
+    [SerializeField] float controlRollFactor = -30.0f;
+
+    float xThrow;
+    float yThrow;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +29,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    public void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * speedFactor;
         float yOffset = yThrow * Time.deltaTime * speedFactor;
@@ -32,7 +45,22 @@ public class PlayerController : MonoBehaviour
         float new_Y_Pos = transform.localPosition.y + yOffset;
 
         float clamped_X_Pos = Mathf.Clamp(new_X_Pos, -xRange, xRange);
-        float clamped_Y_Pos = Mathf.Clamp(new_Y_Pos, -yRange, yRange);
+        float clamped_Y_Pos = Mathf.Clamp(new_Y_Pos, -yRange, yRange + 2);
         transform.localPosition = new Vector3(clamped_X_Pos, clamped_Y_Pos, transform.localPosition.z);
+    }
+
+    public void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControl;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        float yaw = yawDueToPosition;
+
+        float rollDueToControl = xThrow * controlRollFactor;
+        float roll = rollDueToControl;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+
     }
 }
