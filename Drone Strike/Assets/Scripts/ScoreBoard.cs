108acec0b5db66fcd9cae37fc4ccc8c7b9211adc
NexusErexus <uksus924@gmail.com>
2024-02-03 19:57:16 +0300

    Scoreboard in UI

--- Drone Strike/Assets/Scripts/ScoreBoard.cs
+++ Drone Strike/Assets/Scripts/ScoreBoard.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEditor.Search;
 
 public class ScoreBoard : MonoBehaviour
 {
-    int score;
+    TMP_Text scoreText;
+
+    int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GetComponent<TMP_Text>();
+        ShowScore(score);
     }
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
-        Debug.Log(($"Total score: {score}"));
+        ShowScore(score);
+    }
+
+    public string ShowScore(int score)
+    {
+        scoreText.text = "Total score: " + score.ToString();
+        scoreText.fontStyle = FontStyles.UpperCase;
+        return scoreText.text;
     }
 
     
--- Drone Strike/Assets/Scripts/SelfDestruct.cs
+++ Drone Strike/Assets/Scripts/SelfDestruct.cs
@@ -9,6 +9,6 @@ public class SelfDestruct : MonoBehaviour
     {
         Destroy(gameObject, timeTillDestroy);
     }
-
+    
     
 }
